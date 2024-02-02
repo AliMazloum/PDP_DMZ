64aceae4f79d663da6aacb0275b60c2eefbdfe76
Mazloum <AMAZLOUM@email.sc.edu>
2024-02-02 16:43:09 -0500

    Adding the files

---
+++ Measurement_collector/main.py
@@ -0,0 +1,38 @@
+from measurements import Measurements
+from Collecting_threads.collecting_threads import collect_rtt_thread, collect_throughput_thread,collect_throughput_alpha0_thread
+from Collecting_threads.collecting_threads import collect_throughput_alpha1_thread,collect_throughput_alpha2_thread,collect_measurements_thread
+from Collecting_threads.collecting_threads import collect_throughput_alpha3_thread, collect_throughput_alpha4_thread
+from Collecting_threads.collecting_threads import collect_throughput_alpha5_thread, collect_throughput_no_mem_thread
+from Collecting_threads.collecting_threads import collect_throughput_LP_thread, collect_rtt_LP_thread
+from Collecting_threads.collecting_threads import collect_throughput_MuST_thread, collect_rtt_MuST_thread
+import threading
+
+
+collect_measurements_thread.start()
+
+collect_rtt_thread.start()
+
+collect_throughput_thread.start()
+
+collect_throughput_LP_thread.start()
+
+# collect_rtt_LP_thread.start()
+
+collect_throughput_MuST_thread.start()
+
+# collect_rtt_MuST_thread.start()
+
+collect_throughput_alpha0_thread.start()
+
+# collect_throughput_alpha1_thread.start()
+
+# collect_throughput_alpha2_thread.start()
+
+# collect_throughput_alpha3_thread.start()
+
+# collect_throughput_alpha4_thread.start()
+
+# collect_throughput_alpha5_thread.start()
+
+# collect_throughput_no_mem_thread.start()
+
